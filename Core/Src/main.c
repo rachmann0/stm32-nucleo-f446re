e8141b2a7ca52cff5cea748c51c3f8faa1ba5d9a
rachmann0 <reecedarby1889@gmail.com>
2026-06-04 18:25:41 +1000

    turn off TIM6 TIM7

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -145,8 +145,8 @@ int main(void)
   MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
 
-  HAL_TIM_Base_Start_IT(&htim6);
-  HAL_TIM_Base_Start_IT(&htim7);
+  // HAL_TIM_Base_Start_IT(&htim6);
+  // HAL_TIM_Base_Start_IT(&htim7);
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
   // HAL_TIM_Base_Start(&htim6);
 
